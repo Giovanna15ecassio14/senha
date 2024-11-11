4afea85cdb4f26753d8b6b760f01403f559dbd3b
Giovanna15ecassio14 <135229706+Giovanna15ecassio14@users.noreply.github.com>
2024-11-11 10:50:35 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-alert("dsnkjncnsdn kjsndckn");
-let numeroSecreto= 89;
-prompt("sddsvac sadc ");
+alert("Boas-Vindas ao jogo do Número Secreto");
+let numeroSecreto =7 ;
+let chute= prompt("Escolha um número entre 1 e 100");
+
+if (chute== numeroSecreto) {
+    alert('Isso aí, você descobriu o número secreto  ${numeroSecreto}');
+}  else{ 
+    alert("Você errou!")
+}
